0d97358abc375dbd4388e06d4aea1b1c8e63a48a
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-08-10 16:08:55 +0200

    generate frontend and backend projects

---
+++ meduva-backend/src/main/java/com/szusta/meduva/MeduvaApplication.java
@@ -0,0 +1,13 @@
+package com.szusta.meduva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeduvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeduvaApplication.class, args);
+	}
+
+}
---
+++ meduva-backend/src/test/java/com/szusta/meduva/MeduvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szusta.meduva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeduvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
