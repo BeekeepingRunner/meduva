7486a658c468f39e9bd5732763580bb49f9f656a
Bartosz Biegajło <biegbart.z@gmail.com>
2021-08-26 12:48:43 +0200

    delete unnecessary test endpoint access

--- meduva-backend/src/main/java/com/szusta/meduva/security/WebSecurityConfig.java
+++ meduva-backend/src/main/java/com/szusta/meduva/security/WebSecurityConfig.java
@@ -91,7 +91,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/all").permitAll()
-                .antMatchers("/api/password/mail-test").permitAll()
                 .antMatchers("/api/password/request").permitAll()
                 .antMatchers("/api/password/user").permitAll()
                 .antMatchers("/api/test/admin").hasAuthority("ROLE_ADMIN")
