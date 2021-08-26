1caa7f395fabd0da2ce22992215798d6685159fe
Bartosz Biegajło <biegbart.z@gmail.com>
2021-08-26 12:48:21 +0200

    tests up-to-date on this branch

--- meduva-backend/src/test/java/com/szusta/meduva/controller/PasswordResetControllerTest.java
+++ meduva-backend/src/test/java/com/szusta/meduva/controller/PasswordResetControllerTest.java
@@ -1,5 +1,7 @@
 package com.szusta.meduva.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.szusta.meduva.model.User;
 import com.szusta.meduva.service.UserAccountService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -24,9 +27,11 @@ public class PasswordResetControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     @DisplayName("GET /request success")
-    public void shouldReturnOk() throws Exception {
+    public void requestSuccessTest() throws Exception {
 
         String sampleMail = "samplemail@gmail.com";
         doNothing().when(userAccountService).sendResetPasswordEmail(sampleMail);
@@ -38,5 +43,23 @@ public class PasswordResetControllerTest {
                 .andExpect(status().isOk());
 
         verify(userAccountService, times(1)).sendResetPasswordEmail(sampleMail);
+    }
+
+    @Test
+    @DisplayName("POST /user success")
+    public void getUserWithResetTokenSuccessTest() throws Exception {
+
+        String resetToken = "resetToken";
+        when(userAccountService.getUserFromResetToken(resetToken))
+                .thenReturn(new User());
+
+        this.mockMvc.perform(
+                post("/api/password/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(resetToken))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(new User())));
+
+        verify(userAccountService, times(1)).getUserFromResetToken(resetToken);
     }
 }
