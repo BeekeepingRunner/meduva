0db12786b9417f651385e7b9b11e581ef8d39005
Bartosz Biegajło <biegbart.z@gmail.com>
2021-08-27 13:32:04 +0200

    finish front form validation

--- meduva-frontend/src/app/component/password-reset/password-reset.component.ts
+++ meduva-frontend/src/app/component/password-reset/password-reset.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UserService} from "../../service/user.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {samePasswordsValidator} from "../../util/validator/same-passwords";
 
 @Component({
   selector: 'app-password-reset',
@@ -11,33 +13,51 @@ export class PasswordResetComponent implements OnInit {
 
   resetToken: string = '';
   error: string = '';
-  name: string = '';
+  id?: number;
+
+  form!: FormGroup;
+  inputError: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private userService: UserService,
+    private formBuilder: FormBuilder,
   ) { }
 
-  // Identify a user based on a reset token from link which brought him here
   ngOnInit(): void {
+
+    this.form = this.formBuilder.group(
+      {
+        newPass : new FormControl('', [
+          Validators.minLength(8),
+          Validators.maxLength(20)
+        ]),
+        repeatNewPass : new FormControl('')
+      },
+      { validators : samePasswordsValidator });
+
+    this.identifyUserFromToken();
+  }
+
+  identifyUserFromToken() {
     this.route.params.subscribe(
       params => {
         this.resetToken = params['resetToken'];
 
         this.userService.getUserWithResetToken(this.resetToken).subscribe(
           data => {
-            this.name = data.name;
+            this.id = data.id;
           },
           err => {
             this.error = err.error.message;
           }
         );
       }
-    )
+    );
   }
 
   // Send password change request
   onSubmit() {
-
+    
   }
 }
