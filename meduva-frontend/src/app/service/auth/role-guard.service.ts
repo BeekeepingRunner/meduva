79027c8b2826dcdaa3cc8b1a69c304140326d6c3
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-09-06 13:23:45 +0200

    implement auth and role guards

---
+++ meduva-frontend/src/app/service/auth/role-guard.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {AuthService} from "./auth.service";
+import {ActivatedRouteSnapshot, CanActivate, Router} from "@angular/router";
+import {JwtTokenStorageService, TokenUserInfo} from "../token/jwt-token-storage.service";
+import {Role} from "../../model/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuardService implements CanActivate {
+
+  constructor(
+    public auth: AuthService,
+    public jwtStorage: JwtTokenStorageService,
+    public router: Router,
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+
+    const expectedRole = route.data.expectedRole;
+
+    if (!this.isUserAuthorizedWith(expectedRole)) {
+      this.router.navigate(['access-denied']);
+      return false;
+    } else
+      return true;
+  }
+
+  isUserAuthorizedWith(expectedRole: string): boolean {
+    return !this.auth.hasJwtExpired() && this.hasExpectedRole(expectedRole);
+  }
+
+  hasExpectedRole(expectedRole: string): boolean {
+    let user: TokenUserInfo | null = this.jwtStorage.getCurrentUser();
+    if (user != null) {
+      return this.hasRole(user, expectedRole);
+    }
+    else return false;
+  }
+
+  private hasRole(user: TokenUserInfo, expectedRole: string) {
+    let roles: Role[] = user.roles;
+    for(let role of roles) {
+      if (role.name == expectedRole) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
