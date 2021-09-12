865ffa84e73891753a011a5af6b8ab00775e3534
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-09-12 15:27:20 +0200

    add service model and sql

---
+++ meduva-backend/src/main/java/com/szusta/meduva/model/Service.java
@@ -0,0 +1,32 @@
+package com.szusta.meduva.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(
+        name = "service",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "name"),
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    @Column(name = "duration_in_min")
+    private int durationInMin;
+    private float price;
+}
