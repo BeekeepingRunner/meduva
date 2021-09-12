88f8f67b9d64a2f95d7abe21f804bb5e0bfc215c
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-09-12 15:28:07 +0200

    create service repository

---
+++ meduva-backend/src/main/java/com/szusta/meduva/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package com.szusta.meduva.repository;
+
+import com.szusta.meduva.model.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+}
