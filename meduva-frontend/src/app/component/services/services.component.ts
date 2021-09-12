80d7e15262b058fe3f5bfc8ba4d9dbf2eaaf1c74
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-09-12 15:09:13 +0200

    make table frame

--- meduva-frontend/src/app/component/services/services.component.ts
+++ meduva-frontend/src/app/component/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-services',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
+
+  services: any;
+  displayedColumns: string[] = ['name', 'duration', 'price'];
 
   constructor() { }
 
