30170979bf0f6464bef9b6842b0833148bace170
Bartosz Biegajło <biegbart.z@gmail.com>
2021-09-14 11:16:44 +0200

    remove h2, test with test-schema in mysql

--- meduva-backend/src/test/java/com/szusta/meduva/repository/UserRepositoryTest.java
+++ meduva-backend/src/test/java/com/szusta/meduva/repository/UserRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.szusta.meduva.repository;
 
+import com.szusta.meduva.model.ERole;
 import com.szusta.meduva.model.Role;
 import com.szusta.meduva.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,24 +14,27 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepositoryUnderTest;
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Test
     void findAllClientsWithAccount() {
         // given
         User user = new User(
-                1L,
                 "login",
                 "email",
                 "password",
                 "name",
                 "surname",
-                "phoneNumber",
-                Set.of(new Role(1L, "ROLE_CLIENT"))
+                "phoneNumber"
         );
+        user.setRoles(Set.of(roleRepository.getById(ERole.ROLE_CLIENT.getValue())));
         userRepositoryUnderTest.save(user);
 
         // when
