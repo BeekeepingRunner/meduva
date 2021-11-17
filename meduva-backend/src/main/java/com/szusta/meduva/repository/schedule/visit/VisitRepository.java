260f8c05984ec0eb461e4bf5ac6d729f26fcc71c
przemyslawbajda <przemek.android20@gmail.com>
2021-11-17 16:36:54 +0100

    add sql queries to find all visits when user is client or worker

--- meduva-backend/src/main/java/com/szusta/meduva/repository/schedule/visit/VisitRepository.java
+++ meduva-backend/src/main/java/com/szusta/meduva/repository/schedule/visit/VisitRepository.java
@@ -6,6 +6,7 @@ import com.szusta.meduva.repository.undeletable.UndeletableRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -30,4 +31,26 @@ public interface VisitRepository extends UndeletableRepository<Visit> {
     List<Visit> findAllWhereUserIsWorker(long workerId);
 
     List<Visit> findByUnregisteredClient(UnregisteredClient unregisteredClient);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT v.id, v.name, v.time_from, v.time_to, v.description, v.paid, "
+                    + "v.deleted, v.visit_status_id, v.service_id, v.room_id, v.unregistered_client_id  FROM visit v"
+                    + " INNER JOIN user_visit uv ON visit_id = v.id "
+                    + " WHERE as_client = 0 AND uv.user_id = ?1 "
+                    + " AND timestampdiff(MINUTE, time_from, ?2) <= 0 AND timestampdiff(MINUTE, time_to, ?3) >= 0 "
+                    + " ORDER BY v.time_to asc "
+    )
+    List<Visit> findAllWhereUserIsClientBetween(Long clientId, Date startTime, Date endTime);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT v.id, v.name, v.time_from, v.time_to, v.description, v.paid, "
+                    + "v.deleted, v.visit_status_id, v.service_id, v.room_id, v.unregistered_client_id  FROM visit v"
+                    + " INNER JOIN user_visit uv ON visit_id = v.id "
+                    + " WHERE as_client = 1 AND uv.user_id = ?1 "
+                    + " AND timestampdiff(MINUTE, time_from, ?2) <= 0 AND timestampdiff(MINUTE, time_to, ?3) >= 0 "
+                    + " ORDER BY v.time_to asc "
+    )
+    List<Visit> findAllWhereUserIsWorkerBetween(Long workerId, Date startTime, Date endTime);
 }
