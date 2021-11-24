7ccd61cb7789e06317315d1d4fa4ead0b94d9f17
przemyslawbajda <przemek.android20@gmail.com>
2021-11-24 18:53:39 +0100

    add new component

---
+++ meduva-frontend/src/app/component/visit/edit-visit-term/edit-visit-term.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-visit-term',
+  templateUrl: './edit-visit-term.component.html',
+  styleUrls: ['./edit-visit-term.component.css']
+})
+export class EditVisitTermComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
