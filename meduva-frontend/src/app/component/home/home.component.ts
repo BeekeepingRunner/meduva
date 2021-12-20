0330be8362982fc16f438ceec5a0381f2254c37b
Grzegorz <grzegorzbielesza@gmail.com>
2021-12-20 20:07:37 +0100

    Landing page responsivity - working in progress

--- meduva-frontend/src/app/component/home/home.component.ts
+++ meduva-frontend/src/app/component/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../service/user.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,34 @@ import {UserService} from "../../service/user.service";
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+        ]),
+        name: new FormControl('', [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(30),
+          Validators.pattern('^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.\'-]+$'),
+          Validators.pattern('^[^-\\s]+$')
+        ]),
+        phone: new FormControl('', [
+          Validators.pattern('^(\\+[0-9]{1,4})?[0-9]{6,12}$')
+        ]),
+      message: new FormControl('', [
+        Validators.required
+      ])
+      }
+    );
   }
 
 }
