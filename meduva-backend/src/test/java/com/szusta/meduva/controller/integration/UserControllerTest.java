9bf92b2473461ffe47550be9fcf21671afdf89e6
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2022-02-22 17:40:58 +0100

    improve controller test

--- meduva-backend/src/test/java/com/szusta/meduva/controller/integration/UserControllerTest.java
+++ meduva-backend/src/test/java/com/szusta/meduva/controller/integration/UserControllerTest.java
@@ -1,8 +1,10 @@
-package com.szusta.meduva.controller;
+package com.szusta.meduva.controller.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.szusta.meduva.model.User;
 import com.szusta.meduva.service.user.UserService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -31,6 +32,19 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private User user;
+
+    @BeforeEach
+    void setup() {
+        user = new User(
+                "login",
+                "email@mail.com",
+                "password",
+                "name",
+                "surname",
+                "123123123");
+    }
+
     @Nested
     class GetUserTests {
 
@@ -41,20 +55,15 @@ class UserControllerTest {
         )
         public void should_returnUser_when_correctId() throws Exception {
             // given
-            User expectedUser = new User();
-            Long id = 1L;
+            User expectedUser = user;
+            long id = 1L;
             expectedUser.setId(id);
             when(userService.findById(id)).thenReturn(expectedUser);
 
-            // when
-            MvcResult mvcResult = mockMvc.perform(get("/api/user/find/{id}", id))
-                    .andReturn();
-
-            String userJSON = mvcResult.getResponse().getContentAsString();
-            User actual = objectMapper.readValue(userJSON, User.class);
-
-            // then
-            assertEquals(expectedUser.getId(), actual.getId());
+            mockMvc.perform(get("/api/user/find/{id}", id))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.login", CoreMatchers.is(expectedUser.getLogin())))
+                    .andExpect(jsonPath("$.email", CoreMatchers.is(expectedUser.getEmail())));
         }
 
         @Test
